789655d45f401b907c815cfcfdffbb0d5eb1dd84
TacoWarrior24 <tacowarrior24@gmail.com>
2024-09-18 15:24:55 -0400

    Added Styling

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   const data = {
     title: "JLong Blog",
