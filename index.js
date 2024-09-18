f173211d94db53b89979e294a93d7db330d2c7ca
TacoWarrior24 <tacowarrior24@gmail.com>
2024-09-18 13:47:36 -0400

    added about and contact pages

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.get("/", (req, res) => {
   res.render("index.ejs", data);
 });
 
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
